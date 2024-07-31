5b7b5d5a025821fdb2aa29d5364b11343347a9c4
Franque Armoako <frank.amoako-dev@outlook.com>
2024-07-31 18:50:43 +0000

    mini fix

--- src/App.js
+++ src/App.js
@@ -10,57 +10,57 @@ import UpdatePost from "./UpdatePost"
 import axios from "axios"
 
 function App() {
-  const [user, setUser] = useState({})
-  const navigate = useNavigate()
+	const [user, setUser] = useState({})
+	const navigate = useNavigate()
 
-  const token = localStorage.getItem("token")
+	const token = localStorage.getItem("token")
 
-  useEffect(() => {
-    const getUser = async () => {
-      if (!token) {
-        navigate("/login")
-      }
-      if (token) {
-        const response = await axios.get(
-          "/api/v1/get-user-by-id",
+	useEffect(() => {
+		const getUser = async () => {
+			if (!token) {
+				navigate("/login")
+			}
+			if (token) {
+				const response = await axios.get(
+					"/api/v1/get-user-by-id",
 
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        )
-        //console.log(response.data.data)
-        setUser(response.data.data)
-      }
-    }
-    getUser()
-  }, [])
-  return (
-    <div className="">
-      <ToastContainer
-        reverseOrder={false}
-        position="top-center"
-        autoClose={1000}
-        limit={1}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <Routes>
-        <Route path="/" element={user ? <Home /> : <Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post/:slug" element={<Post />} />
-        <Route path="/user/post/:slug" element={<UpdatePost />} />
-      </Routes>
-    </div>
-  )
+					{
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				)
+				//console.log(response.data.data)
+				setUser(response.data.data)
+			}
+		}
+		getUser()
+	}, [token])
+	return (
+		<div className="">
+			<ToastContainer
+				reverseOrder={false}
+				position="top-center"
+				autoClose={1000}
+				limit={1}
+				hideProgressBar={false}
+				newestOnTop
+				closeOnClick={false}
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover
+				theme="dark"
+			/>
+			<Routes>
+				<Route path="/" element={user ? <Home /> : <Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/post/:slug" element={<Post />} />
+				<Route path="/user/post/:slug" element={<UpdatePost />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
